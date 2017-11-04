b3d9d02716a93ffe41b6d4294780f9fcf912ccd9
EBoost-Chair <wgq1995@163.com>
2017-11-04 21:37:26 +0800

    Better Sub,Better Set.
    Now you can set variables with spaces!

--- RBasic.rb
+++ RBasic.rb
@@ -2,7 +2,7 @@
 # RBasic
 # Ruby BASIC complier
 # Need:Define,Var,Call
-require "/root/Desktop/RBASIC/0.01_G/parsecore.rb"
+require "/root/Desktop/RBASIC/0.02_Y/parsecore.rb"
 BasicScirpt=File.read(ARGV[0])
 Basic=BasicScirpt.split(";")
 Basic.each do |loop|
