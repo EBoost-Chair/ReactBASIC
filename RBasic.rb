55c83bcbf98823d326358fa16131689c4aac7ad9
EBoost-Chair <wgq1995@163.com>
2017-11-18 19:32:12 +0800

    Add files via upload

--- RBasic.rb
+++ RBasic.rb
@@ -1,10 +1,19 @@
 #!/usr/bin/ruby -W0
-# RBasic
+# RBasic.rb
 # Ruby BASIC complier
 # Need:Define,Var,Call
-require "/root/Desktop/RBASIC/0.02_G/parsecore.rb"
+require "/root/Desktop/RBASIC/0.02_G/modules/main.rb"
+require "/root/Desktop/RBASIC/0.02_G/core/tokens.rb"
+require "/root/Desktop/RBASIC/0.02_G/core/error.rb"
+if FileTest.exist?(ARGV[0]) != true
+  $NoFileErr.throw(ARGV[0])
+end 
 BasicScirpt=File.read(ARGV[0])
+BasicScirpt.chomp!()
 Basic=BasicScirpt.split(";")
 Basic.each do |loop|
-  Parse_Basic(loop)
+  RBasicMain(loop)
+  if $NoCmd_a == 1 && $NoCmd_b == 1
+    $NoCmdErr.throw(loop)
+  end
 end
