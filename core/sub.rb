0073bf422e1c11641927368a2229146a74f06742
EBoost-Chair <wgq1995@163.com>
2017-11-18 19:43:54 +0800

    Update and rename sub.rb to core/sub.rb

--- core/sub.rb
+++ core/sub.rb
@@ -1,3 +1,15 @@
+
+  def throw(ext)
+    print @name
+    print ":"
+    puts ext
+  end
+  def name=(value)
+    @name=value
+  end
+  def name
+    @name
+  end
 require "/root/Desktop/RBASIC/0.02_G/modules/kernel.rb"
 def RBasicSub(such)
   list=such.split("|")
