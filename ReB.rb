1387e6f508ff58c01fc22b8dbb5fa75dd9c05604
EBoost-Chair <wgq1995@163.com>
2018-02-15 21:51:24 +0800

    1.11y

--- ReB.rb
+++ ReB.rb
@@ -1,6 +1,6 @@
 require "./modules/kernel.rb"
 if ARGV[0] == "-v"
-  puts "ReactBasic 1.10 patch1 (2018-2-11) By RBasicTeam"
+  puts "ReactBasic 1.11 (2018-2-11) By RBasicTeam"
   exit()
 end
 if FileTest.exist?(ARGV[0]) != true
